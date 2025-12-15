ccefc33da7f3847ce3fda2327f445e55a467cd10
ZHU Loïc <loic.zhu@etu.u-pec.fr>
2025-12-15 23:35:24 +0100

    BONNE CHANCE LA CHANCE

---
+++ Lancement.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Lancer un service et enregistrer son PID
+start_service() {
+    local dir=$1
+    cd "$dir" || exit 1
+    mvn spring-boot:run &
+    pid=$!
+    echo "Service dans $dir lancé avec PID $pid"
+    echo $pid >> pids.txt
+    cd - > /dev/null
+}
+
+# Arrêter tous les services enregistrés
+stop_services() {
+    if [ -f pids.txt ]; then
+        while read pid; do
+            echo "Arrêt du service PID $pid"
+            kill $pid
+        done < pids.txt
+        rm pids.txt
+    else
+        echo "Aucun service à arrêter."
+    fi
+}
+
+# Lancer les services
+start_service "/chemin/vers/ms-product"
+echo "Attente 30 secondes pour s'assurer que ms-product est bien lancé"
+sleep 30  # Attendre 30 secondes pour donner du temps au service product
+
+start_service "/chemin/vers/ms-order"
+echo "Attente 30 secondes pour s'assurer que ms-order est bien lancé"
+sleep 30  # Attendre 30 secondes pour donner du temps au service order
+
+start_service "/chemin/vers/ms-membership"
+echo "Attente 30 secondes pour s'assurer que ms-membership est bien lancé"
+sleep 30  # Attendre 30 secondes pour donner du temps au service membership
+
