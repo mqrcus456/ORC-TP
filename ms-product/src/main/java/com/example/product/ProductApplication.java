432b2fe1208fc3336d189069eacf0aa9dce0492f
Adrien Dos Santos <adriend671@gmail.com>
2025-12-15 22:59:16 +0100

    la bd marche mais pas la conn au micro service order

--- ms-product/src/main/java/com/example/product/ProductApplication.java
+++ ms-product/src/main/java/com/example/product/ProductApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrderApplication {
+public class ProductApplication {
     public static void main(String[] args) {
-        SpringApplication.run(OrderApplication.class, args);
+        SpringApplication.run(ProductApplication.class, args);
     }
 }
